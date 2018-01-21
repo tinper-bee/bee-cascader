1b72758c98fac7955d72135b3f6d1763dc3014a3
Boyuzhou <2470200492@qq.com>
2018-01-21 21:42:18 +0800

    fix: 修改示例

---
+++ CHANGELOG.md
@@ -0,0 +1,32 @@
+<a name="1.0.3"></a>
+## 1.0.3 (2018-01-21)
+
+
+### Bug Fixes
+
+* demo展示bug ([5d17320](https://github.com/tinper-bee/bee-cascader/commit/5d17320))
+
+
+### build
+
+* **工具编译:** 工具编译 ([3752fb8](https://github.com/tinper-bee/bee-cascader/commit/3752fb8))
+
+
+### Documentation
+
+* **bee-tools cmd:** bee-tools cmd ([fcfd2e7](https://github.com/tinper-bee/bee-cascader/commit/fcfd2e7))
+
+
+### Tests
+
+* **工具升级:** 工具升级 ([c37b6b9](https://github.com/tinper-bee/bee-cascader/commit/c37b6b9))
+
+
+### BREAKING CHANGES
+
+* **工具编译:** 工具编译
+* **工具升级:** 工具升级
+* **bee-tools cmd:** bee-tools cmd
+
+
+
--- demo/index.js
+++ demo/index.js
@@ -1,10 +1,8 @@
-
-import { Con, Row, Col } from 'bee-layout';
-import { Panel } from 'bee-panel';
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import {Col, Row} from 'bee-layout';
+import {Panel} from 'bee-panel';
 import Button from 'bee-button';
-import Cascader from '../src';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 
 
 const CARET = <i className="uf uf-arrow-down"></i>;
@@ -12,40 +10,44 @@ const CARET = <i className="uf uf-arrow-down"></i>;
 const CARETUP = <i className="uf uf-arrow-up"></i>;
 
 
-var Demo1 = require("./demolist/Demo1");var DemoArray = [{"example":<Demo1 />,"title":" 基础级联菜单","code":"/**\n *\n * @title 基础级联菜单\n * @description\n *\n */\n\nimport React, { Component } from 'react';\nimport {Row, Col } from 'bee-layout';\nimport Cascader from 'bee-cascader';\n\n const options = [\n \t{\n \t  value: '浙江',\n \t  children: [\n \t\t  {\n \t\t    value: '杭州',\n \t\t    children: [\n \t\t\t    {\n \t\t\t      value: '西湖',\n \t\t\t      children: [\n \t\t\t\t    {\n \t\t\t\t      value: '白娘子'\n \t\t\t\t    },\n \t\t\t\t    {\n \t\t\t\t      value: '许仙'\n \t\t\t\t    }]\n \t\t\t    }]\n \t\t  }\n \t  ]\n \t},\n \t{\n \t  value: '江苏',\n \t  children: [\n \t\t  {\n \t\t    value: '南京',\n \t\t    children: [\n \t\t\t    {\n \t\t\t      value: '中华门'\n \t\t\t    }]\n \t\t  }\n \t  ]\n \t},\n \t{\n \t  value: '山东'\n \t}\n ];\n class Demo1 extends Component {\n \trender(){\n \t\treturn(\n            <Row>\n                <Col md={4}>\n                    <div className=\"height-150\">\n                     \t<Cascader options = {options} />\n                    </div>\n                </Col>\n            </Row>\n \t\t)\n \t}\n }\n","desc":""}]
+var Demo1 = require("./demolist/Demo1");var DemoArray = [{"example":<Demo1 />,"title":" 基础级联菜单","code":"/**\n *\n * @title 基础级联菜单\n * @description\n *\n */\n\nimport React, { Component } from 'react';\nimport { Cascader, Row, Col  } from 'tinper-bee';\n\n const options = [\n \t{\n \t  value: '浙江',\n \t  children: [\n \t\t  {\n \t\t    value: '杭州',\n \t\t    children: [\n \t\t\t    {\n \t\t\t      value: '西湖',\n \t\t\t      children: [\n \t\t\t\t    {\n \t\t\t\t      value: '白娘子'\n \t\t\t\t    },\n \t\t\t\t    {\n \t\t\t\t      value: '许仙'\n \t\t\t\t    }]\n \t\t\t    }]\n \t\t  }\n \t  ]\n \t},\n \t{\n \t  value: '江苏',\n \t  children: [\n \t\t  {\n \t\t    value: '南京',\n \t\t    children: [\n \t\t\t    {\n \t\t\t      value: '中华门'\n \t\t\t    }]\n \t\t  }\n \t  ]\n \t},\n \t{\n \t  value: '山东'\n \t}\n ];\n class Demo1 extends Component {\n \trender(){\n \t\treturn(\n            <Row>\n                <Col md={4}>\n                    <div className=\"height-150\">\n                     \t<Cascader options = {options} />\n                    </div>\n                </Col>\n            </Row>\n \t\t)\n \t}\n }\n","desc":""}]
 
 
 class Demo extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             open: false
         }
         this.handleClick = this.handleClick.bind(this);
     }
+
     handleClick() {
-        this.setState({ open: !this.state.open })
+        this.setState({open: !this.state.open})
     }
 
-    render () {
-        const { title, example, code, desc  } = this.props;
+    render() {
+        const {title, example, code, desc, scss_code} = this.props;
         let caret = this.state.open ? CARETUP : CARET;
         let text = this.state.open ? "隐藏代码" : "查看代码";
 
-        const footer = (
-            <Button shape="block" onClick={ this.handleClick }>
-                { caret }
-                { text }
-            </Button>
+        const header = (
+            <div>
+                {example}
+                <Button style={{"marginTop": "10px"}} shape="block" onClick={this.handleClick}>
+                    {caret}
+                    {text}
+                </Button>
+            </div>
         );
-
-
         return (
-            <Col md={12} >
-                <h3>{ title }</h3>
-                <p>{ desc }</p>
-                <Panel collapsible expanded={ this.state.open } colors='bordered' header={ example } footer={footer} footerStyle = {{padding: 0}}>
-                    <pre><code className="hljs javascript">{ code }</code></pre>
+            <Col md={12}>
+                <h3>{title}</h3>
+                <p>{desc}</p>
+                <Panel collapsible headerContent expanded={this.state.open} colors='bordered' header={header}
+                       footerStyle={{padding: 0}}>
+                    <pre><code className="hljs javascript">{code}</code></pre>
+                    {!!scss_code ? <pre><code className="hljs css">{scss_code}</code></pre> : null}
                 </Panel>
             </Col>
         )
@@ -53,20 +55,22 @@ class Demo extends Component {
 }
 
 class DemoGroup extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
     }
-    render () {
+
+    render() {
         return (
-                <Row>
-                    {DemoArray.map((child,index) => {
+            <Row>
+                {DemoArray.map((child, index) => {
 
-                        return (
-                            <Demo example= {child.example} title= {child.title} code= {child.code} desc= {child.desc} key= {index}/>
-                        )
+                    return (
+                        <Demo example={child.example} title={child.title} code={child.code} scss_code={child.scss_code}
+                              desc={child.desc} key={index}/>
+                    )
 
-                    })}
-                </Row>
+                })}
+            </Row>
         )
     }
 }
